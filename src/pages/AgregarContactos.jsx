d637021ac2696102d965c718920692e097cd1187
juliangarciag05 <juliangarciag05@gmail.com>
2025-11-06 03:51:42 +0000

    falta completar crear tarea

---
+++ src/pages/AgregarContactos.jsx
@@ -0,0 +1,10 @@
+import React,{useState} from "react";
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+const AgregarContactos = () => {
+    return(
+        <div>
+            <h1>Hola Mundo</h1>
+        </div>
+    )
+}
+export default AgregarContactos
